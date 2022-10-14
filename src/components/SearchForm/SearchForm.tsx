36d639bc739a0bddf0740bbc8545615ec16841f3
Georgi <georgichichev15@Gmail.com>
2022-10-14 16:15:16 +0300

    Add search form props interface
    Add value and onChange handler to search input

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import styles from './SearchForm.module.css';
 
-const SearchForm = () => {
+interface SearchFormProps{
+    onInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    keyword: string
+}
+
+const SearchForm = ({keyword, onInputChange}: SearchFormProps) => {
     return(
         <form className={styles["searchForm"]}>
             <input
                 type="text"
+                value={keyword}
+                onChange={onInputChange}
             />
             <input
                 type="submit"
