f15bb5865c0d628fcad922dea84c08af8a62c9ae
Georgi <georgichichev15@Gmail.com>
2022-10-14 16:14:48 +0300

    Add search form control
    Create keyword state
    Create search form input change handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import SearchForm from "./components/SearchForm/SearchForm";
 
 function App() {
+    const [keyword, setKeyword] = useState('');
+
+    const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setKeyword(e.target.value);
+    };
+
     return (
         <div className="container">
-
+            <SearchForm
+                onInputChange={onInputChange}
+                keyword={keyword}
+            />
         </div>
     );
 }
