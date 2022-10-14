25ebc0402e4aef310cc5ca7d386a32ff64522208
Georgi <georgichichev15@Gmail.com>
2022-10-14 17:31:57 +0300

    Add useFetch hook
    Create useFetch hook which returns fetch result, loading and error states

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,74 @@
+import { useState, useEffect } from 'react';
+
+interface RentalsState{
+    rentalsData:{
+        id: string
+        name: string
+        imageUrl: string
+    }[]
+}
+
+interface RentalsResult{
+    data: Array<{
+        id: string
+        attributes:{
+            name:string
+        }
+        relationships:{
+            primary_image:{
+                data:{
+                    id: string
+                }
+            }
+        }
+    }>
+    included: Array<{
+        id: string
+        attributes:{
+            url: string
+        }
+    }>
+}
+
+const parseRentalUrl = (id: string, included: RentalsResult["included"]) => {
+    return included.filter(x => x.id === id)[0].attributes.url;
+};
+
+
+function useFetch(url: string) {
+    const [rentalsData, setRentalsData] = useState<RentalsState["rentalsData"]>([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState({});
+
+    useEffect(() => {
+        setLoading(true)
+
+        fetch(url)
+            .then(res => res.json())
+            .then(result => {
+                parseRentalsData(result)
+            })
+            .catch(err => setError(err))
+            .finally(() => setLoading(false));
+    }, [url]);
+
+    const parseRentalsData = (rentalsResult: RentalsResult) =>{
+        const {data, included} = rentalsResult;
+
+        const parsedRentalsData = data.map(rental => {
+            const imageId = rental.relationships.primary_image.data.id;
+
+            return{
+                id: rental.id,
+                name: rental.attributes.name,
+                imageUrl: parseRentalUrl(imageId, included)
+            }
+        })
+
+        setRentalsData(parsedRentalsData);
+    };
+
+    return {rentalsData, loading, error}
+}
+
+export default useFetch;
